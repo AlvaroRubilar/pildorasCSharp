e2470daddb8a046c8f1cdacecd30ebe542043142
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-29 06:50:42 -0300

    video 8 uso método Math.Pow

--- PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/Program.cs
@@ -13,7 +13,7 @@ namespace PrimeraAplicacion
             const double PI = 3.1416;
             Console.WriteLine("Introduce la medida del radio");
             double radio = double.Parse(Console.ReadLine());
-            double area = radio * radio * PI;
+            double area = Math.Pow(radio, 2) * PI;
             Console.WriteLine($"El área del circulo es {area}");
 
 
