2dfe0e3c813904f75abbf390885968057178408b
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-29 15:43:25 -0300

    video 10 mensaje desde un método

---
+++ PracticaMetodos/PracticaMetodos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mesajeEnPantalla();
+        }
+        static void mesajeEnPantalla()
+        {
+            Console.WriteLine("Mensaje desde el método mensajeEnPantalla");
+
+        }
+    }
+}
