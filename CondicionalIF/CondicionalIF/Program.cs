67d7bcf9e25c6afb73d1b94b90b6c2b5af0a74b6
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 20:20:11 -0300

    video 14 variable buleana

---
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool haceFrio;
+            haceFrio = true;
+            Console.WriteLine(!haceFrio);
+
+        }
+    }
+}
