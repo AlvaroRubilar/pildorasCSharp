6a63dd737031e5ee5f9b8dd23b44bc1c789d881a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 19:25:18 -0300

    video 13 extraer método refactorizando

--- PracticaMetodos/PracticaMetodos/Program.cs
+++ PracticaMetodos/PracticaMetodos/Program.cs
@@ -15,6 +15,14 @@ namespace PracticaMetodos
             Console.WriteLine(Suma(7,5.3));
         }
 
-        private static double Suma(int v1, double v2) => v1 + v2;
+        private static double Suma(int v1, double v2)
+        {
+            return Suma2(v1, v2);
+        }
+
+        private static double Suma2(int v1, double v2)
+        {
+            return v1 + v2;
+        }
     }
 }
