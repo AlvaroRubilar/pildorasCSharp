a3e05818601ea088ed45af8abdf42369cecf5f5c
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 19:20:28 -0300

    video 13 creación automática de métodos

--- PracticaMetodos/PracticaMetodos/Program.cs
+++ PracticaMetodos/PracticaMetodos/Program.cs
@@ -14,13 +14,10 @@ namespace PracticaMetodos
         {
             Console.WriteLine(Suma(7,5.3));
         }
-        static int Suma(int operador1, int operador2) => operador1 + operador2;
 
-        static double Suma(double operador1, double operador2) => operador1 + operador2;
-        static int Suma(int numero1, int numero2, int numero3) => numero1 + numero2 + numero3;
-
-
-        static int Suma(int numero1, int numero2, int numero3, int numero4) => numero1 + numero2 + numero3 + numero4;
-
+        private static double Suma(int v1, double v2)
+        {
+            return v1 + v2;
+        }
     }
 }
