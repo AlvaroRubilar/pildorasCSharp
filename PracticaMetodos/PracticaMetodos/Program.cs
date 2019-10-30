30bb4f523485e705792cf8d7c1be90e61afe8967
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 19:34:31 -0300

    video 13 valor opcional a un método

--- PracticaMetodos/PracticaMetodos/Program.cs
+++ PracticaMetodos/PracticaMetodos/Program.cs
@@ -12,17 +12,16 @@ namespace PracticaMetodos
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Suma(7,5.3));
+            int valor1 = 7;
+            double valor2 = 5.2;
+            double valor3 = 8.3;
+
+            Console.WriteLine(Suma(valor1,valor2));
         }
 
-        private static double Suma(int v1, double v2)
+        private static double Suma(int num1, double num2, double num3 = 0)
         {
-            return Suma2(v1, v2);
-        }
-
-        private static double Suma2(int v1, double v2)
-        {
-            return v1 + v2;
+            return num1+num2+num3;
         }
     }
 }
