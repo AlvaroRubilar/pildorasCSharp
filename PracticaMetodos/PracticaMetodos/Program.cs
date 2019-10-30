327fc48caa8a889e07522712dbe456310acdc69d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 19:35:21 -0300

    video 13 uso del parametro opcional

--- PracticaMetodos/PracticaMetodos/Program.cs
+++ PracticaMetodos/PracticaMetodos/Program.cs
@@ -16,7 +16,7 @@ namespace PracticaMetodos
             double valor2 = 5.2;
             double valor3 = 8.3;
 
-            Console.WriteLine(Suma(valor1,valor2));
+            Console.WriteLine(Suma(valor1,valor2, valor3));
         }
 
         private static double Suma(int num1, double num2, double num3 = 0)
