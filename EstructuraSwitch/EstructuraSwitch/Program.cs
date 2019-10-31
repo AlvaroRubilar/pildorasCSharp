21114116fbf29282ebaf857f152bd998f74a2e29
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 23:44:49 -0300

    video 18 switch

---
+++ EstructuraSwitch/EstructuraSwitch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EstructuraSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Elige medio de transporte (coche, tren, avión)");
+            string medioTransporte = Console.ReadLine();
+            switch (medioTransporte)
+            {
+                case "coche":
+                    Console.WriteLine("Velocidad media: 100 km/hr");
+                    break;
+                case "tren":
+                    Console.WriteLine("Velocidad media: 250 km/hr");
+                    break;
+                case "avión":
+                    Console.WriteLine("Velocidad media: 800 km/hr");
+                    break;
+            }
+        }
+    }
+}
