664b3084a9fee3f34134738974561218182d6123
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 22:12:32 -0300

    video 16 otra solución se usa el método String.Compare ignorando mayúsculas

--- CondicionalIF/CondicionalIF/Program.cs
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -10,21 +10,23 @@ namespace CondicionalIF
     {
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Vamos a evaluar si puedes conducir un vehículo");
             Console.WriteLine("Introduce tu edad, por favor");
             int edad = Int32.Parse(Console.ReadLine());
-            string carnet="no";
-            if (edad >= 18)
-            {
-                Console.WriteLine("¿Tienes carnet? si/no");
-                carnet = Console.ReadLine();
-            }
-            if(edad>=18 && carnet=="si")
-                Console.WriteLine("Puedes conducir vehículos");
+            if (edad < 18)
+                Console.WriteLine("No puedes conducir vehículos");
             else
-                Console.WriteLine("No puedes conduir vehículos");
+            {
+                Console.WriteLine("¿Tienes carnet?");
+                string carnet = Console.ReadLine();
+                int compara = String.Compare(carnet, "si", true);
+                if(compara==0)
+                    Console.WriteLine("Puedes conducir vehículos");
+                else
+                    Console.WriteLine("Lo siento mucho no puedes conducir");
 
+            }
         }
     }
 }
