8c4bfb8d1c78f5e092b72af5a9fab47df1f1e80b
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 23:33:52 -0300

    video 17 if anidado

--- CondicionalIF/CondicionalIF/Program.cs
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -14,9 +14,9 @@ namespace CondicionalIF
             Console.WriteLine("Introduce tu edad ");
             int edad = Int32.Parse(Console.ReadLine());
             if (edad < 18) Console.WriteLine("Eres un niño");
-            if (edad < 30) Console.WriteLine( "Eres joven");
-            if (edad < 60) Console.WriteLine("Eres maduro");
-            else Console.WriteLine("Debes cuidarte");
+            else if (edad < 30) Console.WriteLine( "Eres joven");
+            else if (edad < 60) Console.WriteLine("Eres maduro");
+            else Console.WriteLine("Debes cuidarte ya");
 
         }
     }
