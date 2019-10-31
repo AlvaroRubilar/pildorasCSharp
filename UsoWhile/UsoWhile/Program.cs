934fcd2d45f155f467cd7feb78f6c58d8b8e1fb1
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-31 13:18:07 -0300

    video 18 bucle while

---
+++ UsoWhile/UsoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Deseas entrar en un bucle while? si/no");
+            string respuesta = Console.ReadLine();
+            while (respuesta != "no")
+            {
+                Console.WriteLine("Estas ejecutando el interior del bucle while");
+                Console.WriteLine("Introduce tu nombre, por favor");
+                string nombre = Console.ReadLine();
+                Console.WriteLine($"Saldras del bucle {nombre} cuando respondas no a la pregunta" );
+                Console.WriteLine("¿Deseas repetir otra vez? si/no ");
+                respuesta = Console.ReadLine();
+            }
+            Console.WriteLine("Has salido del bucle");
+        }
+    }
+}
