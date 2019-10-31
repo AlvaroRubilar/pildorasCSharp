2a209dcfc138fd490ff5d00a0aab4439b406afdd
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-31 16:43:53 -0300

    video 21 dowhile primero se usa while

---
+++ UsoDoWhile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsoDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 10;
+            while (z<10)
+            {
+                System.Console.WriteLine("Impresión "+10);
+                z++;
+            }
+        }
+    }
+}
