163a00fe82d47e737a19e917b3e96bee47d36357
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-30 22:40:43 -0300

    video 16 operador ||

--- CondicionalIF/CondicionalIF/Program.cs
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -11,13 +11,13 @@ namespace CondicionalIF
         static void Main(string[] args)
         {
             Console.WriteLine("Introduce el primer parcial");
-            int parcial1 = Int32.Parse(Console.ReadLine());
+            float parcial1 = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Introduce el segundo parcial");
-            int parcial2 = Int32.Parse(Console.ReadLine());
+            float parcial2 = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Introduce el tercel parcial");
-            int parcial3 = Int32.Parse(Console.ReadLine());
+            float parcial3 = Int32.Parse(Console.ReadLine());
 
-            if (parcial1 >= 5 && parcial2 >= 5 && parcial3 >= 5)
+            if (parcial1 >= 5 || parcial2 >= 5 || parcial3 >= 5)
                 Console.WriteLine("La nota media es "+(parcial1+parcial2+parcial3)/3);
             else
                 Console.WriteLine("Vuelve en septiembre");
