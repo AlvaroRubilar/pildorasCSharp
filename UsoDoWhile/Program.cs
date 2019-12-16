a7d3ce2739d2ef2bbf42978385e467ddf464df44
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-16 01:32:34 -0300

    video 22 excepciones

--- UsoDoWhile/Program.cs
+++ UsoDoWhile/Program.cs
@@ -15,7 +15,15 @@ namespace UsoDoWhile
             {
                 intentos++;
                 Console.WriteLine("Ingresa un número:");
-                adivina = int.Parse(Console.ReadLine());
+                try{
+
+                    adivina = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException ex){
+
+                    System.Console.WriteLine("No has introducido un número valido, se toma como número intoducido el cero");
+                    adivina=0;
+                }
 
                 Console.WriteLine(adivina < numeroAleatorio ? "Más alto" :
                     (adivina > numeroAleatorio ? "Más bajo" : $"Has acertado en {intentos} intentos"));
