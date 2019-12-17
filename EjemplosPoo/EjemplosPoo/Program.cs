2246624b4c1d88c9fccb0786e4af50372311d66a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 11:17:56 -0300

    video 28 objeto circulo

---
+++ EjemplosPoo/EjemplosPoo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EjemplosPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo;// creación de objeto de tipo circulo. Variable/objeto de tipo círculo
+            
+            miCirculo = new Circulo(); // Iniciación de variable/objeto de tipo Circulo. Instanciar una clase
+                                       // Instanciación. Ejemplarización. Creación de ejemplar de la clase
+
+            Console.WriteLine(miCirculo.calculoArea(5)); 
+            
+
+        }
+
+        class Circulo 
+        {
+            double pi = 3.1416;  // proiedad de la clase Circulo. Campo de clase.
+            public double calculoArea(int radio) //métdo de clase, ¿Qué pueden hacer los objeto de este tipo
+            {
+                return pi * radio * radio;
+
+            }
+
+            
+        }
+    }
+}
