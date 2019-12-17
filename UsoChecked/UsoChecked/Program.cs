2c3bfa2aff72e19d57838c8ff79b5241d33288fd
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 00:06:15 -0300

    video 24 checked hace una excepción

--- UsoChecked/UsoChecked/Program.cs
+++ UsoChecked/UsoChecked/Program.cs
@@ -6,10 +6,13 @@ namespace UsoChecked
     {
         static void Main(string[] args)
         {
+            checked{
+
             int numero = int.MaxValue;
             int resultado = numero + 20;
             
             Console.WriteLine(resultado);
+            }
         }
     }
 }
