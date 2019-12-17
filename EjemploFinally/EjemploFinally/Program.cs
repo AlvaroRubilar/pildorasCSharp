b2adda11eb5e11934400f06f72ac639c559ef93d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 01:16:49 -0300

    video 27 finally siempre se ejecuta

---
+++ EjemploFinally/EjemploFinally/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EjemploFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader archivo = null;
+            try
+            {
+                string linea;
+                int contador = 0;
+                string path = @"C:\Users\mars_\OneDrive\Escritorio\tirar.txt";
+                archivo = new System.IO.StreamReader(path);
+                while((linea = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error con la lectura del archivo");
+            }
+            finally
+            {
+                if (archivo != null) archivo.Close();
+                Console.WriteLine("Conexión con el fichero cerrada");
+            }
+        }
+    }
+}
