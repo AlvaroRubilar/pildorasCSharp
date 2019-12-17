ccb2e6a5a3c9147a93d909c9252356643a8383b0
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 00:02:51 -0300

    video 24 desbordamiento

---
+++ UsoChecked/UsoChecked/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UsoChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.MaxValue;
+            int resultado = numero + 20;
+            
+            Console.WriteLine(resultado);
+        }
+    }
+}
