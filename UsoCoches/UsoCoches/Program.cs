c6b62c088515365363dc79070e16d525640d9746
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 12:55:19 -0300

    video 30 sobrecarga constructor

--- UsoCoches/UsoCoches/Program.cs
+++ UsoCoches/UsoCoches/Program.cs
@@ -11,6 +11,8 @@ namespace UsoCoches
             Coche coche2 = new Coche();
             Console.WriteLine(coche1.getInfoCoche());
             Console.WriteLine(coche2.getInfoCoche());
+            Coche coche3 = new Coche(4500.25, 1.20035);
+            Console.WriteLine(coche3.getInfoCoche());
         }
     }
     class Coche
@@ -20,6 +22,12 @@ namespace UsoCoches
             ruedas = 4;
             largo = 2300.5;
             ancho = 0.800;
+        }
+        public Coche(double largoCoche, double anchoCoche)
+        {
+            ruedas = 4;
+            largo = largoCoche;
+            ancho = anchoCoche;
         }
         public String getInfoCoche()
         {
