6ce53d37a419277d5f8421b770b3d9c3ece445b7
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-17 18:43:51 -0300

    video 32 tareas y otra clase "punto"

---
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConceptosPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void RealizarTarea()
+        {
+            //TODO: se ve en tareas
+        }
+    }
+
+    
+}
