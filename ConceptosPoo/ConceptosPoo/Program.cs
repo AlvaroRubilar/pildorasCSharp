381e414a48851b0b417385db959af8f34ed4d6c4
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-18 00:08:29 -0300

    video 34 se crea otro objeto

--- ConceptosPoo/ConceptosPoo/Program.cs
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -13,6 +13,7 @@ namespace ConceptosPoo
             Punto origen = new Punto(1,2);
             Punto destino = new Punto(6, 14);
             double distancia = origen.DistanciaHasta(destino);
+            Punto otroPunto = new Punto();
             Console.WriteLine($"La distancia entre los dos puntos es de {distancia}");
             Console.WriteLine($"Número de objetos creados: {Punto.ContadorDeObjetos()}");
         }
