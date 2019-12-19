423894c6223e3835798dcce094ef67591c99f9dc
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-18 23:37:30 -0300

    video 35 potencia y raiz

---
+++ ClaseAnonimas/ClaseAnonimas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClaseAnonimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raiz = Math.Sqrt(9);
+            double potencia = Math.Pow(3, 4);
+            Console.WriteLine(raiz);
+            Console.WriteLine(potencia);
+        }
+    }
+}
