54732f5cf132e85743c0eaecefece92005ffcd1f
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-18 23:53:58 -0300

    video 36 imprimir elemento array vacío

---
+++ UsoArray/UsoArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UsoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades;
+            edades = new int[4];
+            Console.WriteLine(edades[2]);
+        }
+    }
+}
