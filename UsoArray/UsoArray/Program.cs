696b4eba876c16dc37b8ebe3ea00b19736e2f13d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-18 23:59:23 -0300

    video 36 llenar arrays

--- UsoArray/UsoArray/Program.cs
+++ UsoArray/UsoArray/Program.cs
@@ -6,8 +6,15 @@ namespace UsoArray
     {
         static void Main(string[] args)
         {
-            int[] edades;
-            edades = new int[4];
+            //int[] edades;
+            //edades =new int[4];
+            //edades[0] = 15;
+            //edades[1] = 27;
+            //edades[2] = 19;
+            //edades[3] = 80;
+
+            //Console.WriteLine(edades[2]);
+            int[] edades = { 15, 27, 19, 80 };
             Console.WriteLine(edades[2]);
         }
     }
