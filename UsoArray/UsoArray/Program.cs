38a13161c87478cfdf100f3f647062dfe9b3f6f5
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-19 00:19:26 -0300

    video 37 distintos tipos de array

--- UsoArray/UsoArray/Program.cs
+++ UsoArray/UsoArray/Program.cs
@@ -6,16 +6,33 @@ namespace UsoArray
     {
         static void Main(string[] args)
         {
-            //int[] edades;
-            //edades =new int[4];
-            //edades[0] = 15;
-            //edades[1] = 27;
-            //edades[2] = 19;
-            //edades[3] = 80;
-
-            //Console.WriteLine(edades[2]);
-            int[] edades = { 15, 27, 19, 80 };
-            Console.WriteLine(edades[2]);
+            //array implícito
+            var datos = new[] { "Alvaro", "Rubilar", "Chile" };//deben ser del mismo tipo
+            var valores = new[] { 15, 28, 35, 75.5, 30.3 };//se convierte en double
+            //array de objetos
+            Empleados Ana = new Empleados("Ana", 27);
+            Empleados[] arrayEmpleados = new Empleados[2];
+            arrayEmpleados[0] = new Empleados("Sara", 37);
+            arrayEmpleados[1] = Ana;
+            //array de tipos de o clases anónimas
+            var personas = new[]
+            {
+                new {Nombre= "Juan", Edad=19 },
+                new {Nombre= "María", Edad= 49},
+                new {Nombre= "Diana", Edad= 35}
+            };
+            Console.WriteLine(personas[1]);
         }
+    }
+    class Empleados
+    {
+        public Empleados(String nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            
+        }
+        String nombre;
+        int edad;
     }
 }
