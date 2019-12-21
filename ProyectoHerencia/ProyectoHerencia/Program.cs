f84a0a04521bac9ee66e61dcf93f26b55508e1ef
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-20 23:43:26 -0300

    video 43 :base() y método getNombre

--- ProyectoHerencia/ProyectoHerencia/Program.cs
+++ ProyectoHerencia/ProyectoHerencia/Program.cs
@@ -6,15 +6,20 @@ namespace ProyectoHerencia
     {
         static void Main(string[] args)
         {
-            Caballo Babieca = new Caballo();
-            Humano Alvaro = new Humano();
-            Gorila Copito = new Gorila();
+            Caballo Babieca = new Caballo("Babieca");
+            Humano Alvaro = new Humano("Alvaro");
+            Gorila Copito = new Gorila("Copito");
             Copito.trepar();
+            Alvaro.getNombre();
         }
     }
 
     class Mamiferos
     {
+        public Mamiferos(String nombre)
+        {
+            nombreSerVivo = nombre;
+        }
         public void respirar()
         {
             Console.WriteLine("Soy capaz de respirar");
@@ -25,16 +30,30 @@ namespace ProyectoHerencia
         {
             Console.WriteLine("Cuido de mis crias hasta que se valgan por si misma");
         }
+        public void getNombre()
+        {
+            Console.WriteLine($"El nombre del ser vivo es: {nombreSerVivo}");
+        }
+        public String nombreSerVivo;
     }
     class Caballo : Mamiferos
     {
-    public void galopar()
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+        }
+
+        public void galopar()
         {
             Console.WriteLine("Soy capaz de galopar");
         }
     }
     class Humano : Mamiferos
     {
+        public Humano(String nombreHumano) : base(nombreHumano)
+        {
+
+        }
         public void pensar()
         {
             Console.WriteLine("Soy capaz de pensar");
@@ -43,6 +62,10 @@ namespace ProyectoHerencia
 
     class Gorila : Mamiferos
     {
+        public Gorila(String nombreGorila) : base(nombreGorila)
+        {
+
+        }
         public void trepar()
         {
             Console.WriteLine("Soy capaz de trepar");
