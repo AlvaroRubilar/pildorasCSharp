50c65d6993a8f1d64d4278be820b38d7de2bef7d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-29 13:05:48 -0300

    video 55 ceación clase empleado

---
+++ PropiedadesAcceso/PropiedadesAcceso/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PropiedadesAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Alvaro = new Empleado("Álvaro");
+
+        }
+    }
+
+    class Empleado
+    {
+
+
+        public Empleado(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void setSalario(double salario)
+        {
+
+            if (salario < 0)
+            {
+                Console.WriteLine("El salario no puede ser negativo. Se asignará 0 como salario");
+                this.salario = 0;
+            }
+            else
+            {
+                this.salario = salario;
+            }
+        }
+        public double getSalario()
+        {
+
+            return salario;
+        }
+        private string nombre;
+        private double salario;
+
+
+    }
+}
