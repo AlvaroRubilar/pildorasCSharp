224686431138aa37b039163be015dfb47f672875
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-29 13:32:13 -0300

    video 56 propertie con lambda

--- PropiedadesAcceso/PropiedadesAcceso/Program.cs
+++ PropiedadesAcceso/PropiedadesAcceso/Program.cs
@@ -8,8 +8,8 @@ namespace PropiedadesAcceso
         {
             Empleado Alvaro = new Empleado("Álvaro");
             Alvaro.SALARIO = -750;
-            Console.WriteLine("El salario del empleado es: "+Alvaro.SALARIO);
-          
+            Console.WriteLine("El salario del empleado es: " + Alvaro.SALARIO);
+
 
         }
     }
@@ -44,24 +44,24 @@ namespace PropiedadesAcceso
         {
             if (salario < 0) return 0;
             else return salario;
-            
-           
+
+
         }
         //creación de propiedad
-        public double SALARIO
-        {
-            get
-            {
-                return this.salario;
-            }
-            set
-            {
-                this.salario = evaluaSalario(value);
-            }
-        }
+        //public double SALARIO
+        //{
+        //    get
+        //    {
+        //        return this.salario;
+        //    }
+        //    set
+        //    {
+        //        this.salario = evaluaSalario(value);
+        //    }
+        //}
         private string nombre;
         private double salario;
 
-
+        public double SALARIO { get => this.salario; set => this.salario = evaluaSalario(value); }
     }
 }
