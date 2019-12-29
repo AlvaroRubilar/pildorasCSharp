7a06f6be1de2e82799289631bc210172c0e1431b
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-29 18:30:39 -0300

    video 57 cambia salario

--- StructsYEnum/StructsYEnum/Program.cs
+++ StructsYEnum/StructsYEnum/Program.cs
@@ -7,24 +7,29 @@ namespace StructsYEnum
         static void Main(string[] args)
         {
             Empleado empleado1 = new Empleado(1200, 250);
+            empleado1.cambiaSalario(empleado1,100);
             Console.WriteLine(empleado1);
         }
     }
 
-    internal class Empleado
+     public  class Empleado
     {
-        private int salarioBase;
-        private int comision;
+        private double salarioBase;
+        private double comision;
 
-        public Empleado(int salarioBase, int comision)
+        public Empleado(double salarioBase, double comision)
         {
-            this.SalarioBase = salarioBase;
-            this.Comision = comision;
+            this.salarioBase = salarioBase;
+           this.comision = comision;
         }
 
-        public int SalarioBase { get => salarioBase; set => salarioBase = value; }
-        public int Comision { get => comision; set => comision = value; }
+       
         public override string ToString() =>
             string.Format( "Salario y comisión del empleado ({0},{1})", salarioBase, comision);
+        public void cambiaSalario (Empleado emp,double incremento)
+        {
+            emp.salarioBase += incremento;
+            emp.comision += incremento;
+        }
     }
 }
