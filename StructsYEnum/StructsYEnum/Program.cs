6caa5fe8c61487806ea8b2ef5155549fe9db0a94
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-29 21:15:06 -0300

    video 58 declaración enum estaciones

--- StructsYEnum/StructsYEnum/Program.cs
+++ StructsYEnum/StructsYEnum/Program.cs
@@ -2,34 +2,15 @@
 
 namespace StructsYEnum
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Empleado empleado1 = new Empleado(1200, 250);
-            empleado1.cambiaSalario(empleado1,100);
-            Console.WriteLine(empleado1);
+            
         }
     }
 
-     public  struct Empleado
-    {
-        private double salarioBase;
-        private double comision;
+    enum Estaciones { Primavera, Verano, Otoño, Invierno};
 
-        public Empleado(double salarioBase, double comision)
-        {
-            this.salarioBase = salarioBase;
-           this.comision = comision;
-        }
-
-       
-        public override string ToString() =>
-            string.Format( "Salario y comisión del empleado ({0},{1})", salarioBase, comision);
-        public void cambiaSalario (Empleado emp,double incremento)
-        {
-            emp.salarioBase += incremento;
-            emp.comision += incremento;
-        }
-    }
 }
