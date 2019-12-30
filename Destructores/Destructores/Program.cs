7610ff762209447fc087b4928efd8ceddd46bd5a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-29 22:10:43 -0300

    video 59 destructores

---
+++ Destructores/Destructores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Destructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miArchivo = new ManejoArchivos();
+            miArchivo.mensaje();
+        }
+    }
+    class ManejoArchivos
+    {
+        StreamReader archivo = null;
+        int contador = 0;
+        string linea;
+        public ManejoArchivos()
+        {
+            archivo = new StreamReader(@"C:\Users\mars_\OneDrive\Documents\Prueba.txt");
+            while ((linea=archivo.ReadLine())!=null)
+            {
+                Console.WriteLine(linea);
+                contador++;
+            }
+        }
+        public void mensaje()
+        {
+            Console.WriteLine("hay {0} lineas",contador);
+            
+        }
+        ~ManejoArchivos()
+        {
+            archivo.Close();
+        }
+    }
+}
