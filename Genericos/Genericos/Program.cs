489270a21ed02ed3354ba0647610fbb77f0ea5e6
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 23:40:36 -0300

    video 60 creación de array de objetos

---
+++ Genericos/Genericos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmacenObjeros archivos = new AlmacenObjeros(4);
+            archivos.agregar("Alvaro");
+            archivos.agregar("Elena");
+            archivos.agregar("Antonio");
+            archivos.agregar("Sandra");
+            String nombrePersona = (String)archivos.getElemento(2);
+            Console.WriteLine(nombrePersona);
+        }
+    }
+
+    class AlmacenObjeros
+    {
+        public AlmacenObjeros(int z)
+        {
+            datosElemento = new Object[z];
+        }
+        public void agregar(Object obj)
+        {
+
+            datosElemento[i] = obj;
+            i++;
+        }
+        public Object getElemento(int i)
+        {
+            return datosElemento[i];
+        }
+        private Object[] datosElemento;
+        private int i = 0;
+    }
+}
