531f9cf204204bff8a1a02e24bf62f9030b04b78
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-02 00:40:03 -0300

    video 62 clase generica restringida a 3 empleados

---
+++ GenericosRestricciones/GenericosRestricciones/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace GenericosRestricciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            almacenEmpleados<Director> empleados = new almacenEmpleados<Director>(3);
+            empleados.agregar(new Director(4500));
+            empleados.agregar(new Director(1500));
+            empleados.agregar(new Director(3500));
+        }
+    }
+    class almacenEmpleados <T> where T : IParaEmpleados
+    {
+       
+        public almacenEmpleados(int z)
+        {
+            datosEmpleado = new T[z];
+        }
+        public void agregar(T obj)
+        {
+            datosEmpleado[i] = obj;
+            i++;
+        }
+        public T getEmpleado(int i)
+        {
+            return datosEmpleado[i];
+        }
+        private int i = 0;
+        private T[] datosEmpleado;
+
+    }
+    class Director:IParaEmpleados
+    {
+        private double salario;
+
+        
+
+        public Director(double salario)
+        {
+            
+            this.salario = salario;
+        }
+        public double  getSalario()
+        {
+            return salario;
+        }
+    }
+    class Secretaria : IParaEmpleados
+    {
+
+        private double salario;
+
+
+
+        public Secretaria(double salario)
+        {
+
+            this.salario = salario;
+        }
+        public double getSalario()
+        {
+            return salario;
+        }
+    }
+
+    class Electricista : IParaEmpleados
+    {
+        private double salario;
+
+
+
+        public Electricista(double salario)
+        {
+
+            this.salario = salario;
+        }
+        public double getSalario()
+        {
+            return salario;
+        }
+    }
+
+    interface IParaEmpleados
+    {
+        double getSalario();
+    }
+}
