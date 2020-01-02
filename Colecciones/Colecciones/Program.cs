28fb3ecca42ac8b0186a29298ade552db95dc095
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-02 12:50:45 -0300

    video 63 creación lista

---
+++ Colecciones/Colecciones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();// Creación Lista
+            numeros.Add(5);
+            numeros.Add(7);
+            numeros.Add(9);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
