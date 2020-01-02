5e2f8805ed0c6d5a5bcce4aa9d05e7aa79f0e7b5
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-02 13:07:33 -0300

    video 62 ingreso por teclado lista

--- Colecciones/Colecciones/Program.cs
+++ Colecciones/Colecciones/Program.cs
@@ -8,15 +8,20 @@ namespace Colecciones
         static void Main(string[] args)
         {
             List<int> numeros = new List<int>();// Creación Lista
-            int[] listaNumeros = new int[] { 3, 6, 8, 10, 50 };
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("¿Cuántos elemntos quieres introducir?");
+            int elem = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < elem; i++)
             {
-                numeros.Add(listaNumeros[i]);
+                Console.Write($"Ingresa el elemento {i}: ");
+                numeros.Add(Int32.Parse(Console.ReadLine()));
             }
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("Elementos Introducidos");
+            for (int i = 0; i < elem; i++)
             {
                 Console.WriteLine(numeros[i]);
             }
+          
         }
     }
 }
