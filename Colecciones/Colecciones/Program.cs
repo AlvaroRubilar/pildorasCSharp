dd41acdd0f1a68ac36efb606b0081f0246c2cd28
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-02 23:22:22 -0300

    video 65 creación e impresión elementos de la cola

--- Colecciones/Colecciones/Program.cs
+++ Colecciones/Colecciones/Program.cs
@@ -7,24 +7,22 @@ namespace Colecciones
     {
         static void Main(string[] args)
         {
-            LinkedList<int> numeros = new LinkedList<int>();
+            Queue<int> numeros = new Queue<int>();
+            // rellenando o agregando elementos a la cola
 
-            foreach(int num in new int[] { 10, 8, 6, 4, 2, 0 })
+            foreach (int num in new int[5] { 2, 4, 6, 8, 10 })
             {
-                numeros.AddLast(num);
+                numeros.Enqueue(num);
             }
-            //foreach (int num in numeros)
-            //{
-            //    Console.WriteLine(num);
-            //}
-            //numeros.Remove(6);
-            LinkedListNode<int> nodoImportante = new LinkedListNode<int>(15);
-            numeros.AddFirst(nodoImportante);
-            for (LinkedListNode<int> nodo = numeros.First; nodo != null; nodo = nodo.Next)
+            //recorriendo la cola
+
+            Console.WriteLine("Recorriendo el Queue");
+
+            foreach (int num in numeros)
             {
-                int numero = nodo.Value;
-                Console.WriteLine(numero);
+                Console.WriteLine(num);
             }
+            
         }
     }
 }
