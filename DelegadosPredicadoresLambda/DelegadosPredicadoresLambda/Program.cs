f124b25bee2bfd9c030a89a0a1c00b0184bd99d9
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-04 16:49:04 -0300

    video 69 método ForEach

--- DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
+++ DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
@@ -21,7 +21,8 @@ namespace DelegadosPredicadosLambda
 
             List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             List<int> numerosPares = numeros.FindAll(i => i % 2 == 0);
-            foreach (int numero in numerosPares) Console.WriteLine(numero);
+            //foreach (int numero in numerosPares) Console.WriteLine(numero);
+            numerosPares.ForEach(numero => Console.WriteLine(numero));
 
 
         }
