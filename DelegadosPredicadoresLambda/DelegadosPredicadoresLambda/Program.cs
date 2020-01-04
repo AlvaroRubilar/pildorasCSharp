a6c42150f306bbbab3c17182b5db4c5d7fdf2126
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-04 12:40:47 -0300

    video 67 delegado Saludo Bienvenida

---
+++ DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegadosPredicadosLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creción del objeto delegado apuntando a MensajeBienvenida
+            ObjDelegado ElDelegado = new ObjDelegado(MensajeBienvenida.SaludoBienvenida);
+            ElDelegado();
+        }
+        // Definición del objeto delegado
+        delegate void ObjDelegado();
+    }
+    class MensajeBienvenida
+    {
+        public static void SaludoBienvenida()
+        {
+            Console.WriteLine("Hola acabo de llegar.¿Qué tal?");
+        }
+    }
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida()
+        {
+            Console.WriteLine("Bueno ya me marcho. Hasta luego");
+        }
+    }
+}
