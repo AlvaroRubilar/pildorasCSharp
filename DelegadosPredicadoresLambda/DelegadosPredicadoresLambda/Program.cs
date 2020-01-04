4987412735434d74958eec8f7951a2b73beee585
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-04 15:10:18 -0300

    video 69 creación delegado y función Cuadrado

--- DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
+++ DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
@@ -8,28 +8,27 @@ namespace DelegadosPredicadosLambda
         static void Main(string[] args)
         {
             List<Personas> gente = new List<Personas>();
-            Personas P1 = new Personas();
-            P1.Nombre = "Antonio";
-            P1.Edad = 40;
 
-            Personas P2 = new Personas();
-            P2.Nombre = "María";
-            P2.Edad = 28;
+            //Personas P1 = new Personas();
+            //P1.Nombre = "Antonio";
+            //P1.Edad = 40;
 
-            Personas P3 = new Personas();
-            P3.Nombre = "Ana";
-            P3.Edad = 37;
+            //Personas P2 = new Personas();
+            //P2.Nombre = "María";
+            //P2.Edad = 28;
 
-            gente.AddRange(new Personas[] { P1, P2, P3 });
-
-            Predicate<Personas> elPredicado = new Predicate<Personas>(ExistenMayoresDeEdad);
-
-            bool existe = gente.Exists(elPredicado);
-            Console.WriteLine(existe ? "Hay Personas mayores de edad":"No hay nadie mayor de edad") ;
+            //Uso del delgado
+            OperacionesMatematicas operacion = new OperacionesMatematicas(Cuadrado);
+            Console.WriteLine(operacion(4));
+           
 
 
         }
-        static bool ExistenMayoresDeEdad(Personas persona) => persona.Edad>=18 ? true : false;
+        public delegate int OperacionesMatematicas(int numero);
+        public static int Cuadrado(int num)
+        {
+            return num * num;
+        }
         
         
     }
