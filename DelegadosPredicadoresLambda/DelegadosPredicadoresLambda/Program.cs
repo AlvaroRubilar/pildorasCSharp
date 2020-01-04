b6b9a52f7185944fd7312d285c0420a8f60d905a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-04 14:54:41 -0300

    video 68 predicado con objeto personas

--- DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
+++ DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
@@ -7,21 +7,40 @@ namespace DelegadosPredicadosLambda
     {
         static void Main(string[] args)
         {
-            List<int> listaNumeros = new List<int>();
-            listaNumeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            List<Personas> gente = new List<Personas>();
+            Personas P1 = new Personas();
+            P1.Nombre = "Alvaro";
+            P1.Edad = 40;
 
-            //Declaramos delegado predicado
-            Predicate<int> elDelegadoPred = new Predicate<int>(DamePares);
-            List<int> numPares = listaNumeros.FindAll(elDelegadoPred);
-            foreach (var num in numPares)
-               
-            {
-                Console.WriteLine(num);
+            Personas P2 = new Personas();
+            P2.Nombre = "María";
+            P2.Edad = 28;
 
-            }
+            Personas P3 = new Personas();
+            P3.Nombre = "Ana";
+            P3.Edad = 37;
+
+            gente.AddRange(new Personas[] { P1, P2, P3 });
+
+            Predicate<Personas> elPredicado = new Predicate<Personas>(ExisteAlvaro);
+
+            bool existe = gente.Exists(elPredicado);
+            Console.WriteLine(existe ? "Hay Personas con el nombre Alvaro":"No hay nadie llamado Alvaro") ;
+
+
         }
-       static bool DamePares(int num)=> num % 2 == 0 ? true : false;
+        static bool ExisteAlvaro(Personas persona) => persona.Nombre == "Alvaro" ? true : false;
         
+        
+    }
+
+    class Personas
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
     }
     
 }
