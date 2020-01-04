86b2534e5786c33fe252cb5d24c11ca9d99fedfd
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-04 16:58:48 -0300

    video 69 comparar edades de personas

--- DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
+++ DelegadosPredicadoresLambda/DelegadosPredicadoresLambda/Program.cs
@@ -9,23 +9,21 @@ namespace DelegadosPredicadosLambda
         {
             List<Personas> gente = new List<Personas>();
 
-            //Personas P1 = new Personas();
-            //P1.Nombre = "Antonio";
-            //P1.Edad = 40;
+            Personas P1 = new Personas();
+            P1.Nombre = "Antonio";
+            P1.Edad = 40;
 
-            //Personas P2 = new Personas();
-            //P2.Nombre = "María";
-            //P2.Edad = 28;
-
+            Personas P2 = new Personas();
+            P2.Nombre = "María";
+            P2.Edad = 28;
+            ComparaPersonas comparaEdad = (persona1, persona2) => persona1 == persona2;
+            Console.WriteLine(comparaEdad(P1.Edad,P2.Edad));
             
 
-            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            List<int> numerosPares = numeros.FindAll(i => i % 2 == 0);
-            //foreach (int numero in numerosPares) Console.WriteLine(numero);
-            numerosPares.ForEach(numero => { Console.WriteLine("El número par es:"); Console.WriteLine(numero); });
 
 
         }
+        public delegate bool ComparaPersonas(int edad1, int edad2);
         
         
     }
