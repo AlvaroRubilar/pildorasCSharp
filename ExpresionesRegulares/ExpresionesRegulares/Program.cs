7951d4fcdfa223de96d57c5ec87f48f5ec8ea914
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-08 13:10:55 -0300

    video 71 expresiones para una web

--- ExpresionesRegulares/ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -7,12 +7,12 @@ namespace ExpresionesRegulares
     {
         static void Main(string[] args)
         {
-            string frase = "Mi nombre es juan y mi n° de tfno es (+47)123-45-67 (+34)123-45-67 y mi código postal es 29679";
-            string patron = @"\+34";
+            string frase = "Mi web es http://pildorasinformaticas.es";
+            string patron = "https?://(www.)?pildorasinformaticas.es";
             Regex miRegex = new Regex(patron);
             MatchCollection elMatch = miRegex.Matches(frase);
 
-            Console.WriteLine(elMatch.Count > 0? "Se ha encotrado número de teléfono de España":"No se ha encontrado número de teléfono de España");
+            Console.WriteLine(elMatch.Count > 0? "Se ha encontrado web":"No se ha encontrado web");
         }
     }
 }
