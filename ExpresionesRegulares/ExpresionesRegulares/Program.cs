c4df5ac010ddd4d9c2b50e2659c2b916c4dcff8d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-08 12:35:50 -0300

    video 70 busqueda de número de telefono

--- ExpresionesRegulares/ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -8,11 +8,11 @@ namespace ExpresionesRegulares
         static void Main(string[] args)
         {
             string frase = "Mi nombre es juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
-            string patron = @"\d";
+            string patron = @"\(\+\d{2}\)\d{3}-\d{2}-\d{2}";
             Regex miRegex = new Regex(patron);
             MatchCollection elMatch = miRegex.Matches(frase);
 
-            Console.WriteLine(elMatch.Count > 0? "Se han encotrado números":"No se ha encontrado números");
+            Console.WriteLine(elMatch.Count > 0? "Se ha encotrado número de teléfono":"No se ha encontrado número de teléfono");
         }
     }
 }
