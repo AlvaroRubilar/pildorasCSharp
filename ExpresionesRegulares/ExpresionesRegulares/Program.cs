2a347ec2d4fa9f7beffe201478973fdb71f14459
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-08 12:38:15 -0300

    video 70 busqueda número de España

--- ExpresionesRegulares/ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -8,11 +8,11 @@ namespace ExpresionesRegulares
         static void Main(string[] args)
         {
             string frase = "Mi nombre es juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
-            string patron = @"\(\+\d{2}\)\d{3}-\d{2}-\d{2}";
+            string patron = @"\+34";
             Regex miRegex = new Regex(patron);
             MatchCollection elMatch = miRegex.Matches(frase);
 
-            Console.WriteLine(elMatch.Count > 0? "Se ha encotrado número de teléfono":"No se ha encontrado número de teléfono");
+            Console.WriteLine(elMatch.Count > 0? "Se ha encotrado número de teléfono de España":"No se ha encontrado número de teléfono de España");
         }
     }
 }
