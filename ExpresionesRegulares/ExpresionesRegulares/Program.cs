f984399d9160a103de596bea0b13604b2f90f2b8
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-08 12:26:20 -0300

    video 70 prueba con j

--- ExpresionesRegulares/ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -7,12 +7,12 @@ namespace ExpresionesRegulares
     {
         static void Main(string[] args)
         {
-            string frase = "Mi nombre es Juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
+            string frase = "Mi nombre es juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
             string patron = "[J]";
             Regex miRegex = new Regex(patron);
             MatchCollection elMatch = miRegex.Matches(frase);
 
-            Console.WriteLine(elMatch.Count > 0? "Se ha encontrado una J":"No se ha encontrado");
+            Console.WriteLine(elMatch.Count > 0? "Se ha encontrado una J":"No se ha encontrado J");
         }
     }
 }
