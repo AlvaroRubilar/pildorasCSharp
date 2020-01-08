c58928fc26c08ad92b27ddc61fc1090a3fb99c05
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-08 12:31:20 -0300

    video 70 busqueda de números

--- ExpresionesRegulares/ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -8,11 +8,11 @@ namespace ExpresionesRegulares
         static void Main(string[] args)
         {
             string frase = "Mi nombre es juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
-            string patron = "[J]";
+            string patron = @"\d";
             Regex miRegex = new Regex(patron);
             MatchCollection elMatch = miRegex.Matches(frase);
 
-            Console.WriteLine(elMatch.Count > 0? "Se ha encontrado una J":"No se ha encontrado J");
+            Console.WriteLine(elMatch.Count > 0? "Se han encotrado números":"No se ha encontrado números");
         }
     }
 }
